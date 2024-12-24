49704f8348e195702f107c01f05c90b7bad6e53f
Sakshi Pokhriyal <sakshi.pokhriyal007@gmail.com>
2024-12-24 17:46:08 +0530

    Add files via upload

---
+++ Module1.py
@@ -0,0 +1,17 @@
+x = 100
+def add(a,b):
+    add=a+b
+    return (add)
+
+def mul(a,b):
+    mul=a*b
+    return (mul)
+
+def sub(a,b):
+    sub = a-b
+    return(sub)
+
+def div(a,b):
+    div = a/b
+    return(div)
+    
---
+++ importEx2.py
@@ -0,0 +1,2 @@
+import Module1
+print(Module1.add(5,10))
